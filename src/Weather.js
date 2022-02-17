9e66e81ce036e1f352fc4a64bd16cfe96cdc63d7
Trinidad <89329249+trinidadmrg@users.noreply.github.com>
2022-02-17 17:52:48 -0300

    API Fix

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "3e43443f02e74f827281e5c5dd6ef4f9";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -32,7 +32,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "3e43443f02e74f827281e5c5dd6ef4f9";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
